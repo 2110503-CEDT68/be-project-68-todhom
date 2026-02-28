f6798453fb5e9e926b66f55cd4cfdc7dad32864e
oat123th-sudo <oat123th@gmail.com>
2026-02-28 19:45:36 +0700

    routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, getMe } = require("../controllers/auth");
+const { register, login, getMe, logout } = require("../controllers/auth"); // เพิ่ม logout
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ const { protect } = require("../middleware/auth");
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", logout); // ระบบต้องยอมให้ User log out ได้
 router.get("/me", protect, getMe);
 
 module.exports = router;
